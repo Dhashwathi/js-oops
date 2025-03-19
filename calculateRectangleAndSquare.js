9f8536e952b1217d8a24c371a5cf5718179908ab
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-03-19 11:30:32 +0530

    Add- Class for calculate square area and perimeter

--- calculateRectangleAndSquare.js
+++ calculateRectangleAndSquare.js
@@ -11,7 +11,23 @@ class Rectangle{
     }
 }
 
+class Square{
+    constructor(side){
+        this.side=side;
+    }
+    calculateSquareArea(){
+        return this.side*this.side;
+    }
+    calculateSquarePerimeter(){
+        return 4*(this.side);
+    }
+}
+
 const rectangle8x4 =new Rectangle(8,4);
 //const rectangle2x8 =new Rectangle(2,8);
 console.log(rectangle8x4.calculateArea());
 console.log(rectangle8x4.calculatePerimeter());
+
+const sqaure =new Square(3);
+console.log(`Area of the sqaure: ${sqaure.calculateSquareArea()}`);
+console.log(`Perimeter of the sqaure: ${sqaure.calculateSquarePerimeter()}`);
