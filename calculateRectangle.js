77afd8a10eb8809ff9a1bc64817b702ecaf22847
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-03-19 11:22:40 +0530

    update- OOPS concept is used to calculate rectangle area and rectangle

--- calculateRectangle.js
+++ calculateRectangle.js
@@ -1,7 +1,17 @@
-function calculate(l,w){
-    console.log(l*w);
+class Rectangle{
+    constructor(length,width){
+        this.length=length;
+        this.width=width;
+    }
+    calculateArea(){
+        return this.length*this.width;
+    }
+    calculatePerimeter(){
+        return 2*(this.length+this.width);
+    }
 }
 
-let length=10;
-let width=5;
-calculate(length,width);
+const rectangle8x4 =new Rectangle(8,4);
+//const rectangle2x8 =new Rectangle(2,8);
+console.log(rectangle8x4.calculateArea());
+console.log(rectangle8x4.calculatePerimeter());
